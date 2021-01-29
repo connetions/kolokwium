5e7cddfb24c496faf39e6046aca30666be0a6a69
tomasz.tomzynski <tomasz.tomzynski@smcebi.edu.pl>
2021-01-29 14:11:31 +0100

    Zadanie3

---
+++ build/CMakeFiles/myProgram.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myProgram.dir/address.o"
+  "CMakeFiles/myProgram.dir/employee.o"
+  "CMakeFiles/myProgram.dir/main.o"
+  "myProgram.pdb"
+  "myProgram"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myProgram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
